baeab84b49fb08026328b1bc9b22b764657c7eab
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-04-08 11:11:50 +0200

    add test wrong format

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -34,8 +34,8 @@ describe("Given I am connected as an employee", () => {
           files: [new File(["image.png"], "image.png", { type: "image/png" })],
         },
       })
-      expect(handleChangeFile).toHaveBeenCalled();
-      expect(inputFile.files[0].name).toBe("image.png");
+      expect(handleChangeFile).toHaveBeenCalled()
+      expect(inputFile.files[0].name).toBe("image.png")
     })
   })
 })
@@ -43,6 +43,39 @@ describe("Given I am connected as an employee", () => {
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page and I submit the form width an image (jpg, jpeg, png)", () => {
     test("Then it should create a new bill", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      };
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+      );
+      const firestore = null;
+      const html = NewBillUI();
+      document.body.innerHTML = html
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage: window.localStorage,
+      });
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      const submitBtn = screen.getByTestId("form-new-bill");
+      submitBtn.addEventListener("submit", handleSubmit);
+      fireEvent.submit(submitBtn);
+      expect(handleSubmit).toHaveBeenCalled();
+    })
+  })
+})
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page and I add a file other than an image (jpg, jpeg or png)", () => {
+    test("Then, the bill shouldn't be created and I stay on the NewBill page", () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
@@ -57,8 +90,7 @@ describe("Given I am connected as an employee", () => {
       );
       const firestore = null;
       const html = NewBillUI();
-      document.body.innerHTML = html;
-
+      document.body.innerHTML = html
       const newBill = new NewBill({
         document,
         onNavigate,
@@ -66,10 +98,12 @@ describe("Given I am connected as an employee", () => {
         localStorage: window.localStorage,
       });
       const handleSubmit = jest.fn(newBill.handleSubmit);
-      const submitBtn = screen.getByTestId("form-new-bill");
-      submitBtn.addEventListener("submit", handleSubmit);
-      fireEvent.submit(submitBtn);
-      expect(handleSubmit).toHaveBeenCalled();
+      newBill.fileName = "invalid"
+      const submitBtn = screen.getByTestId("form-new-bill")
+      submitBtn.addEventListener("submit", handleSubmit)
+      fireEvent.submit(submitBtn)
+      expect(handleSubmit).toHaveBeenCalled()
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy()
     })
   })
 })
