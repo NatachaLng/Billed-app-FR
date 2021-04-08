daf6fb56fa4bf61813b172081e4ad54a69bb68a3
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-04-08 10:31:35 +0200

    add bill icon test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,18 +3,34 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import '@testing-library/jest-dom/extend-expect'
 import Bills  from "../containers/Bills.js"
-import { ROUTES } from "../constants/routes"
+import { ROUTES, ROUTES_PATH } from "../constants/routes"
+import Router from "../app/Router"
 import firebase from "../__mocks__/firebase"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 import userEvent from '@testing-library/user-event'
-
+import Firestore from "../app/Firestore"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+      jest.mock("../app/Firestore");
+      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() });
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+      );
+      const pathname = ROUTES_PATH["Bills"];
+      Object.defineProperty(window, "location", { value: { hash: pathname } });
+      document.body.innerHTML = `<div id="root"></div>`;
+      Router();
+      expect(
+          screen.getByTestId("icon-window").classList.contains("active-icon")
+      ).toBe(true);
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({data: bills})
